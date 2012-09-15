1fdae376303ff28e2edc22242cc7814f90cc572c
Bjørnar Hansen <tilbjornar@gmail.com>
2012-09-15 09:46:57 +0200

    xmonad: fix warnings, whitespace. Colour ws label when in use. Add Skype window handling

--- xmonad.hs
+++ xmonad.hs
@@ -1,5 +1,5 @@
 -- XMonad config file
--- 
+--
 -- By Bjørnar Hansen <tilbjornar@gmail.com>
 --
 -- XMonad version 0.9.2
@@ -7,34 +7,23 @@
 {-- imports --}
 import XMonad hiding ( (|||) )
 import System.Exit
-import System.IO (hPutStrLn)
+import System.IO (Handle)
 
 import XMonad.Util.Run
 import XMonad.Util.EZConfig
-import XMonad.Util.Loggers
 
-import XMonad.Layout hiding ( (|||) )
-import XMonad.Layout.LayoutModifier
 import XMonad.Layout.LayoutCombinators
-import XMonad.Layout.ResizableTile
 import XMonad.Layout.LayoutHints
 import XMonad.Layout.ToggleLayouts
 import XMonad.Layout.NoBorders
-import XMonad.Layout.TwoPane
-import XMonad.Layout.Tabbed
 import XMonad.Layout.IM
 import XMonad.Layout.Grid
-import XMonad.Layout.PerWorkspace
 import XMonad.Layout.WindowNavigation
-import XMonad.Layout.Named
-import XMonad.Layout.DwmStyle
-import XMonad.Layout.Accordion
-import qualified XMonad.Layout.Magnifier as Mag
 
 import XMonad.Prompt
 import XMonad.Prompt.Shell
 
-import XMonad.Hooks.EwmhDesktops 
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks   -- avoidstruts
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.DynamicLog hiding (xmobar, xmobarPP, xmobarColor, sjanssenPP, byorgeyPP)
@@ -49,30 +38,39 @@ import qualified Data.Map         as M
 import qualified XMonad.StackSet  as W
 
 import qualified SolarizedColors  as Color
+import Data.Maybe
+import Data.Monoid (Endo)
 
 {-- Theme / color settings --}
 
 -- define some fonts
+fontInconsolata :: String
 fontInconsolata = "xft:Inconsolata:size=19"
+
+fontDroidSansMono :: String
 fontDroidSansMono = "xft:Droid Sans Mono Dotted:size = 14"
 
+background, foreground, border :: String
 background = Color.base03
 foreground = Color.base0
 border     = Color.green
 
+myFocusedBorderColor, myNormalBorderColor :: String
 myFocusedBorderColor = border
 myNormalBorderColor  = Color.base01
 
+dzenFont, dzenBgColor, dzenFgColor :: String
 dzenFont    = drop 4 fontInconsolata  --dzen does not support `xft:` when xmonad launches it
 dzenBgColor = background
 dzenFgColor = foreground
 
 -- Shell prompt theme
+myShellPrompt :: XPConfig
 myShellPrompt = defaultXPConfig
-        {   
+        {
                 font              = fontInconsolata,
                 bgColor           = background,
-                fgColor           = foreground, 
+                fgColor           = foreground,
                 fgHLight          = Color.base03,
                 bgHLight          = Color.yellow,
                 borderColor       = border,
@@ -83,8 +81,9 @@ myShellPrompt = defaultXPConfig
         }
 
 -- Pretty printing for logHook
+myPP :: Handle -> PP
 myPP h = defaultPP
-    {   ppCurrent         = dzenColor Color.base03 Color.base00 . pad . wsName, 
+    {   ppCurrent         = \wsId -> dzenColor Color.base03 (ppMultiColor wsId) . pad $ wsName wsId,
         ppHidden          = pad . (\wsId ->  dzenColor (ppMultiColor wsId) "" (wsName wsId)),
         ppHiddenNoWindows = dzenColor Color.base01 "" . pad . wsName,
         ppLayout          = dzenColor foreground "" . pad,
@@ -95,11 +94,8 @@ myPP h = defaultPP
         ppOutput          = hPutStrLn h
         -- ppExtras = logLoad : L.date ("^pa(1250)^bg() %a, %b %d ^fg(white)%H:%M^fg()") : []
     }
-    where ppMultiColor wsId = case (M.lookup wsId wsColorMap) of
-                                        Nothing    -> Color.base1
-                                        Just color -> color
-                                            
-          wsName wsId = case (M.lookup wsId wsNameMap) of
+    where ppMultiColor wsId = fromMaybe Color.base1 (M.lookup wsId wsColorMap)
+          wsName wsId = case M.lookup wsId wsNameMap of
                                 Nothing    -> wsId
                                 Just name  -> wsId ++ ":" ++ name
 
@@ -107,10 +103,10 @@ myPP h = defaultPP
 {-- Workspaces --}
 
 myWorkspaces :: [WorkspaceId]
-myWorkspaces = map show [1..9] ++ ["0"]
+myWorkspaces = map show [1..9::Int] ++ ["0","+"]
 
 myWorkspaceNames :: [WorkspaceId]
-myWorkspaceNames = 
+myWorkspaceNames =
         [ "sys", "web", "com", "mus", "log" ]
 
 wsNameMap :: M.Map WorkspaceId WorkspaceId
@@ -119,9 +115,9 @@ wsNameMap = M.fromList $ zip myWorkspaces myWorkspaceNames
 wsNameToId :: M.Map WorkspaceId WorkspaceId
 wsNameToId = M.fromList $ zip myWorkspaceNames myWorkspaces
 
--- Workspace colors map 
-wsColorMap :: M.Map WorkspaceId [Char] 
-wsColorMap = 
+-- Workspace colors map
+wsColorMap :: M.Map WorkspaceId String
+wsColorMap =
         M.fromList $ zip myWorkspaces
             [ Color.yellow
             , Color.orange
@@ -134,11 +130,12 @@ wsColorMap =
 
 {-- Key bindings --}
 --TODO: add focusUrgent keybind
-myKeys = \conf -> mkKeymap conf $
-    [ 
+myKeys :: XConfig l -> M.Map (KeyMask, KeySym) (X ())
+myKeys conf = mkKeymap conf $
+    [
         -- Spawn applications
-        ("M-S-<Return>" , spawn $ XMonad.terminal conf)        , 
-        ("M-l"          , shellPrompt myShellPrompt)           , 
+        ("M-S-<Return>" , spawn $ XMonad.terminal conf)        ,
+        ("M-l"          , shellPrompt myShellPrompt)           ,
         ("M-<F12>"      , spawn "/home/anachron/bin/udsks.sh") ,
 
         -- Multimedia keys
@@ -160,21 +157,21 @@ myKeys = \conf -> mkKeymap conf $
         ("M-k"  , spawn "/usr/bin/xcalib -invert -alter") ,
 
         -- Move focus
-        ("M-n" , sendMessage $ Go L) , 
-        ("M-e" , sendMessage $ Go D) , 
-        ("M-i" , sendMessage $ Go R) , 
-        ("M-u" , sendMessage $ Go U) , 
+        ("M-n" , sendMessage $ Go L) ,
+        ("M-e" , sendMessage $ Go D) ,
+        ("M-i" , sendMessage $ Go R) ,
+        ("M-u" , sendMessage $ Go U) ,
 
         -- Move focus (on floats)
         ("M-<Page_Down>", windows W.focusDown),
         ("M-<Page_Up>"  , windows W.focusUp  ),
 
         -- Move / swap windows
-        ("M-S-n" , sendMessage $ Swap L) , 
-        ("M-S-e" , sendMessage $ Swap D) , 
-        ("M-S-i" , sendMessage $ Swap R) , 
-        ("M-S-u" , sendMessage $ Swap U) , 
-        
+        ("M-S-n" , sendMessage $ Swap L) ,
+        ("M-S-e" , sendMessage $ Swap D) ,
+        ("M-S-i" , sendMessage $ Swap R) ,
+        ("M-S-u" , sendMessage $ Swap U) ,
+
         -- Resize master area
         ("M-C-e" , sendMessage Shrink) ,
         ("M-C-u" , sendMessage Expand) ,
@@ -187,26 +184,27 @@ myKeys = \conf -> mkKeymap conf $
         ("M-z"   , spawn "xscreensaver-command -lock"),
 
         -- Quit or reload XMonad
-        ("M-S-q" , io (exitWith ExitSuccess)),
+        ("M-S-q" , io exitSuccess),
         ("M-q"   , broadcastMessage ReleaseResources >> restart "xmonad" True)
     ]
     ++
     -- "M-[1..9,0,-]" -- Switch to workspace N
     -- "M-S-[1..9,0,-]" -- Move client to workspace N
     -- "M-C-[1..9,0,-]" -- Copy client to workspace N
-    [("M-" ++ m ++ [k], windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) (['1' .. '9'] ++ ['0', '-'])
+    [("M-" ++ m ++ k, windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) myWorkspaces
         , (f, m) <- [ (W.greedyView, ""), (W.shift, "S-"), (copy, "C-") ]
     ]
     ++
     -- "M-C-S-[1..9,0,-]" -- Move client to workspace N and follow
-    [("M-C-S-" ++ [k], (windows $ W.shift i) >> (windows $ W.greedyView i))
-        | (i, k) <- zip (XMonad.workspaces conf) (['1' .. '9'] ++ ['0', '-'])
+    [("M-C-S-" ++ k, windows (W.shift i) >> windows (W.greedyView i))
+        | (i, k) <- zip (XMonad.workspaces conf) myWorkspaces
     ]
 
 {--  Hooks --}
 
-myManageHook = (composeAll
+myManageHook :: Query (Endo WindowSet)
+myManageHook = composeAll
     [ className      =? "stalonetray"        --> doIgnore
     , className      =? "Skype"              --> doShift ( workspace "com" )
     , className      =? "Kopete"             --> doShift ( workspace "com" )
@@ -214,14 +212,13 @@ myManageHook = (composeAll
     , className      =? "Tomahawk"           --> doShift ( workspace "mus" )
     , className      =? "Firefox"            --> doShift ( workspace "web" )
     , title          =? "glxgears"           --> doFloat
+    , title          =? "TSP"                --> doFloat
     , className      =? "fontforge"          --> doFloat
     , className      =? "St80"               --> doFloat -- MetaEdit+
     , title          =? "MetaEdit+ Startup Launcher"      --> doFloat -- MetaEdit+ (wine)
     , isFullscreen                           --> doFullFloat
-    ]) <+> manageDocks
-    where workspace wsName = case (M.lookup wsName wsNameToId) of
-                                Nothing     -> "1" --failsafe
-                                Just id     -> id
+    ] <+> manageDocks
+    where workspace wsName = fromMaybe "1" $ M.lookup wsName wsNameToId
 
 myLayoutHook =  avoidStruts
                 -- . windowNavigation
@@ -233,14 +230,18 @@ myLayoutHook =  avoidStruts
                 $ myLayouts
                 where
                     myLayouts   = tiled ||| Mirror tiled ||| Grid
+                                   ||| withIM 0.22 isSkype Grid
+                    isSkype = Or (Title "anachron88 - Skype™")(Title "Skype™ 2.1 (Beta) for Linux")
                     tiled       = Tall nmaster delta ratio
                     nmaster     = 1
                     ratio       = 1/2
                     delta       = 3/100
 
+myLogHook :: Handle -> X ()
 myLogHook dzpipe =
         dynamicLogWithPP (myPP dzpipe) >> updatePointer (Relative 0.95 0.95)
 
+statusBarCmd :: String
 statusBarCmd = "dzen2" ++
                " -bg '" ++ dzenBgColor ++ "'" ++
                " -fg '" ++ dzenFgColor ++ "'" ++
@@ -249,10 +250,10 @@ statusBarCmd = "dzen2" ++
                " -h 16 -x 0 -y 0 -ta l -e 'onstart=lower'"
 
 main :: IO ()
-main = do 
+main = do
     dzpipe <- spawnPipe statusBarCmd
     xmonad $ withUrgencyHook NoUrgencyHook defaultConfig    -- xmonad $ ewmh defaultconfig
-        { 
+        {
             handleEventHook    = fullscreenEventHook,
             borderWidth        = 2,
             normalBorderColor  = myNormalBorderColor,
